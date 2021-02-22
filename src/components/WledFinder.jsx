bf26f496216765da30cad30d7a0205888425c4f1
Paco Croket <croketproduction@gmail.com>
2021-02-22 19:02:09 +0100

    show spinner while searching for wled devices

--- src/components/WledFinder.jsx
+++ src/components/WledFinder.jsx
@@ -4,10 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getWledEffects, getWledPalettes } from '../http/wledHandler';
 import { setWledDevice } from '../store/actions';
 import WledDevice from './WledDevice';
+import { CgSpinner } from 'react-icons/cg';
 
 export default function WledFinder() {
   const [effects, setEffects] = useState([]);
   const [palettes, setPalettes] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
   const localIp = useSelector((store) => store.localIp);
   const wledDevices = useSelector((store) => store.wledDevices);
   const dispatch = useDispatch();
@@ -17,8 +19,10 @@ export default function WledFinder() {
   }, [localIp]);
 
   const findWled = async () => {
+    setIsSearching(true);
     console.log('finding Wled devices');
     // range is [0-255] but 0 and 255 are reserved
+    const promises = [];
     for (var i = 2; i < 254; i++) {
       try {
         const ipArray = localIp.split('.');
@@ -26,7 +30,12 @@ export default function WledFinder() {
         ipArray.push(i);
         const ip = ipArray.join('.');
 
-        axios('http://' + ip + '/json/info').then((res) => {
+        const promise = axios('http://' + ip + '/json/info');
+
+        promises.push(promise); // to keep track of when all promises are done
+
+        // we check the result immediately anyway
+        promise.then((res) => {
           if (res.statusText === 'OK') {
             const body = res.data;
             dispatch(setWledDevice({ ...body, ip, controlGroupId: null })); //  TODO util to construct a new wled device
@@ -37,13 +46,10 @@ export default function WledFinder() {
         console.log(err.response);
       }
     }
+    Promise.all(promises).then((responses) => {
+      setIsSearching(false);
+    });
   };
-
-  // on mount
-  //   useEffect(() => {
-  //     // search for Wled devices in local network
-  //     findWled();
-  //   }, []);
 
   // get effect and palette list
   useEffect(() => {
@@ -60,12 +66,24 @@ export default function WledFinder() {
   }, [wledDevices]);
 
   return (
-    <div className="devices-grouping  rounded-l-lg border border-r-0 border-white">
+    <div className="devices-grouping rounded-l-lg border border-r-0 border-white flex flex-col justify-start items-center">
       <h1>WLED Finder</h1>
 
-      <button onClick={findWled}>Find</button>
-
-      {Object.keys(wledDevices).length > 0 ? (
+      <button
+        className="p-1 my-2 bg-indigo-800 hover:bg-indigo-600 rounded w-20"
+        onClick={findWled}
+      >
+        <div className="flex justify-between content-center mx-2">
+          Find
+          <span className="ml-2 align-middle">
+            {isSearching && <CgSpinner className="animate-spin" size="1.5em" />}
+          </span>
+        </div>
+      </button>
+      <span>
+        {wledDevices.length > 0 ? `${wledDevices.length}` : 'No'} WLED devices
+      </span>
+      {Object.keys(wledDevices).length > 0 && (
         <div>
           {Object.entries(wledDevices).map(([ip, device]) => (
             <WledDevice
@@ -76,8 +94,6 @@ export default function WledFinder() {
             />
           ))}
         </div>
-      ) : (
-        <div>No WLED devices</div>
       )}
     </div>
   );

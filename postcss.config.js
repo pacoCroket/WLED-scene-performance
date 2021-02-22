d2f47cb891e16ff47c414de43683b10492821621
Paco Croket <croketproduction@gmail.com>
2021-02-22 19:01:34 +0100

    postcss config

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const cssnano = require('cssnano');
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+  plugins: [
+    cssnano(),
+    postcssPresetEnv(),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+};
